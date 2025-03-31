7c2e6fc464987ac5ffb055db93154bd98a8a06f6
AndreasBardram <105062795+AndreasBardram@users.noreply.github.com>
2025-03-31 15:29:33 -0500

    update

--- room_finder/lib/screens/home_screen.dart
+++ room_finder/lib/screens/home_screen.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   Future<void> _uploadTest() async {
     try {
-      await FirebaseFirestore.instance.collection('test').add({
-        'message': 'Hello from Flutter!',
-        'timestamp': DateTime.now().toUtc().toString(),
-      });
-      debugPrint('Successfully uploaded test document!');
+      //await FirebaseFirestore.instance.collection('test').add({
+        //'message': 'Hello from Flutter!',
+        //'timestamp': DateTime.now().toUtc().toString(),
+      //});
+      //debugPrint('Successfully uploaded test document!');
     } catch (e) {
       debugPrint('Error uploading test document: $e');
     }
