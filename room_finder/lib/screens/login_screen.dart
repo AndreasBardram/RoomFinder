535f730cfb54e39c346a534e9b47d9f120dbddf4
AndreasBardram <105062795+AndreasBardram@users.noreply.github.com>
2025-04-28 17:45:00 -0500

    login screen update

--- room_finder/lib/screens/login_screen.dart
+++ room_finder/lib/screens/login_screen.dart
@@ -65,110 +65,105 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Optional background gradient
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.white,
-              Colors.blue.shade50,
-            ],
-          ),
-        ),
-        child: SafeArea(
-          child: GestureDetector(
-            onTap: () => FocusScope.of(context).unfocus(), // Hide keyboard on tap
-            child: Center(
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // Email field
-                    TextField(
-                      controller: _usernameController,
-                      decoration: customInputDecoration(labelText: 'Email'),
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyMedium
-                          ?.copyWith(fontSize: 14),
-                    ),
-                    const SizedBox(height: 16),
-                    // Password field
-                    TextField(
-                      controller: _passwordController,
-                      decoration: customInputDecoration(labelText: 'Password'),
-                      obscureText: true,
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyMedium
-                          ?.copyWith(fontSize: 14),
-                    ),
-                    const SizedBox(height: 24),
-                    // Log In button
-                    CustomButtonContainer(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15.0),
-                        child: ElevatedButton(
-                          style: customElevatedButtonStyle(),
-                          onPressed: _login,
-                          child: Text(
-                            'Log In',
-                            style: Theme.of(context)
-                                .textTheme
-                                .headlineSmall
-                                ?.copyWith(fontSize: 16),
+      // Background color now just the primary color
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(), // Hide keyboard on tap
+          child: Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Email field
+                  TextField(
+                    controller: _usernameController,
+                    decoration: customInputDecoration(labelText: 'Email'),
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(fontSize: 14),
+                  ),
+                  const SizedBox(height: 16),
+                  // Password field
+                  TextField(
+                    controller: _passwordController,
+                    decoration: customInputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(fontSize: 14),
+                  ),
+                  const SizedBox(height: 24),
+                  // Buttons aligned horizontally with minimal space
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Log In button
+                      CustomButtonContainer(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: ElevatedButton(
+                            style: customElevatedButtonStyle(),
+                            onPressed: _login,
+                            child: Text(
+                              'Log In',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineSmall
+                                  ?.copyWith(fontSize: 16),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 16),
-                    // Create Account button navigates to the CreateAccountScreen
-                    CustomButtonContainer(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15.0),
-                        child: ElevatedButton(
-                          style: customElevatedButtonStyle(),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const CreateAccountScreen()),
-                            );
-                          },
-                          child: Text(
-                            'Create Account',
-                            style: Theme.of(context)
-                                .textTheme
-                                .headlineSmall
-                                ?.copyWith(fontSize: 16),
+                      const SizedBox(width: 5), // Minimal space between buttons
+                      // Create Account button
+                      CustomButtonContainer(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: ElevatedButton(
+                            style: customElevatedButtonStyle(),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const CreateAccountScreen(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              'Create Account',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineSmall
+                                  ?.copyWith(fontSize: 16),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 16),
-                    // Skip button for testing
-                    CustomButtonContainer(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15.0),
-                        child: ElevatedButton(
-                          style: customElevatedButtonStyle(),
-                          onPressed: _skipLogin,
-                          child: Text(
-                            'Skip',
-                            style: Theme.of(context)
-                                .textTheme
-                                .headlineSmall
-                                ?.copyWith(fontSize: 16),
+                      const SizedBox(width: 5), // Minimal space between buttons
+                      // Skip button
+                      CustomButtonContainer(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: ElevatedButton(
+                            style: customElevatedButtonStyle(),
+                            onPressed: _skipLogin,
+                            child: Text(
+                              'Skip',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineSmall
+                                  ?.copyWith(fontSize: 16),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
